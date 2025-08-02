6fecc53d1fd78f98c1d8ab4b9b47a1864cc3d282
standard-chan <starp321@naver.com>
2025-08-02 19:17:16 +0900

    feat: branch 출력 변경 및 색상 추가

--- command/BranchCommand.js
+++ command/BranchCommand.js
@@ -1,34 +1,52 @@
 import fs from 'fs';
 import path from 'path';
+import chalk from 'chalk';
+import GitUtil from '../GitUtil.js';
 
-export default class branchCommand {
+export default class BranchCommand {
   constructor(rootPath) {
     this.rootPath = rootPath;
-    this.gitPath = path.join(rootPath, ".git");
-    this.refsHeadsPath = path.join(this.gitPath, "refs", "heads");
+    this.gitPath = path.join(rootPath, '.git');
+    this.refsHeadsPath = path.join(this.gitPath, 'refs', 'heads');
+    this.gitUtil = GitUtil.getInstance();
   }
 
   /** 현재 브랜치 목록을 출력 */
   printBranchList() {
-    const branches = fs.readdirSync(this.refsHeadsPath);
+    const currentBranch = this.gitUtil.getCurrentBranch();
+    try {
+      const branches = fs.readdirSync(this.refsHeadsPath);
 
-    console.log('브랜치 목록:');
-    branches.forEach(branch => {console.log(`- ${branch}`);});
-    console.log();
+      console.log(chalk.bold('브랜치 목록'));
+      branches.forEach(branch => {
+        if (branch == currentBranch) console.log(` ${chalk.greenBright('➤')} ${chalk.greenBright(branch)} (current)`);
+        else console.log(` ${chalk.cyanBright('➤')} ${chalk.cyan(branch)}`);
+      });
+      console.log();
+    } catch (err) {
+      console.error(chalk.red(`브랜치 목록을 불러오는 중 오류 발생: ${err.message}`));
+    }
   }
 
   /** refs/heads에 브랜치 파일 생성 */
   createBranch(name, commitHash) {
     const newBranchPath = path.join(this.refsHeadsPath, name);
     const branchDir = path.dirname(newBranchPath);
-    
-    // 브랜치 디렉토리가 존재하지 않으면 생성
-    if (!fs.existsSync(branchDir)) {
-      fs.mkdirSync(branchDir, { recursive: true });
+
+    try {
+      // 브랜치 디렉토리가 존재하지 않으면 생성
+      if (!fs.existsSync(branchDir)) {
+        fs.mkdirSync(branchDir, { recursive: true });
+      }
+
+      fs.writeFileSync(newBranchPath, commitHash + '\n');
+
+      console.log(
+        chalk.green('✓ ') +
+        `${chalk.cyanBright(name)} 브랜치를 생성하였습니다. `
+      );
+    } catch (err) {
+      console.error(chalk.red(`브랜치 생성 실패: ${err.message}`));
     }
-    
-    fs.writeFileSync(newBranchPath, commitHash + '\n');
-    console.log(`${name} 브랜치를 생성하였습니다.`);
   }
-  
 }
